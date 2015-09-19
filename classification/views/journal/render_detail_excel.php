b8134b22b054ac570dcbbd369c09703ba7d5d0ed
Mohd Hafiz <apiztol@gmail.com>
2015-09-19 15:30:35 +0800

    update export pdf & excel

--- classification/views/journal/render_detail_excel.php
+++ classification/views/journal/render_detail_excel.php
@@ -31,28 +31,30 @@ $objPHPExcel->setActiveSheetIndex(0)->mergeCells('B2:C2');
 $objPHPExcel->getActiveSheet()->SetCellValue('B2', 'Journal Detail');
 $objPHPExcel->getActiveSheet()->getStyle("B2:B2")->getFont()->setBold(true);
 
+$objPHPExcel->getActiveSheet()->getColumnDimension('C')->setWidth('100');
+$objPHPExcel->getActiveSheet()->getStyle('C')->getAlignment()->setWrapText(true);
+
 $objPHPExcel->getActiveSheet()->SetCellValue('B4', 'Title:');
 $objPHPExcel->getActiveSheet()->SetCellValue('B5', 'Dicipline:');
 $objPHPExcel->getActiveSheet()->SetCellValue('B6', 'Publisher:');
 $objPHPExcel->getActiveSheet()->SetCellValue('B7', 'Year Evaluate:');
 $objPHPExcel->getActiveSheet()->SetCellValue('B8', 'Form:');
 $objPHPExcel->getActiveSheet()->SetCellValue('B9', 'Score:');
-$objPHPExcel->getActiveSheet()->SetCellValue('B10', 'Level:');
 
 $objPHPExcel->getActiveSheet()->SetCellValue('C4', $journal['journal_name']);
 $objPHPExcel->getActiveSheet()->SetCellValue('C5', $disciplineTitle);
 $objPHPExcel->getActiveSheet()->SetCellValue('C6', $journal['publisher']);
-$objPHPExcel->getActiveSheet()->SetCellValue('C7', $journal['year']);
+$objPHPExcel->getActiveSheet()->SetCellValue('C7', $journal['year'] . " ");
 $objPHPExcel->getActiveSheet()->SetCellValue('C8', $_GET['f']);
 $objPHPExcel->getActiveSheet()->SetCellValue('C9', $journal['totalMarks'] . ' / ' . $fullMarks . ' (' . round(($journal['totalMarks'] / $fullMarks) * 100, 2) . '%)');
-$objPHPExcel->getActiveSheet()->SetCellValue('C10', '-');
 
 
 $objPHPExcel->getActiveSheet()->SetCellValue('B12', '');
 $objPHPExcel->getActiveSheet()->SetCellValue('C12', 'Criteria');
 $objPHPExcel->getActiveSheet()->SetCellValue('D12', 'Choice');
 $objPHPExcel->getActiveSheet()->SetCellValue('E12', 'Score');
-$objPHPExcel->getActiveSheet()->SetCellValue('F12', 'Remarks');
+$objPHPExcel->getActiveSheet()->SetCellValue('F12', 'Percentage');
+$objPHPExcel->getActiveSheet()->SetCellValue('G12', 'Remarks');
 
 
 $x = 0 ;
@@ -64,19 +66,17 @@ foreach ($journal['resultList'] as $row):
 	$objPHPExcel->getActiveSheet()->SetCellValue('B'.$i, $x);
 	$objPHPExcel->getActiveSheet()->SetCellValue('C'.$i, $row['criteria_name']);
 	$objPHPExcel->getActiveSheet()->SetCellValue('D'.$i, $row['choice_name']);
-	$objPHPExcel->getActiveSheet()->SetCellValue('E'.$i, $row['marks']);
-	$objPHPExcel->getActiveSheet()->SetCellValue('F'.$i, $row['remarks']);
+	$objPHPExcel->getActiveSheet()->SetCellValue('E'.$i, $row['marks'] . ' / ' . $row['totalCriteriaMarks']);
+	$objPHPExcel->getActiveSheet()->SetCellValue('F'.$i, round($row['marks'] / $row['totalCriteriaMarks'] * 100, 2) . '%');
+	$objPHPExcel->getActiveSheet()->SetCellValue('G'.$i, $row['remarks']);
 
-	array_push($scores, [ 'value' => ($row['marks'] / $fullMarks * 100)]);
-	
 endforeach;
 
-$scores = array_reverse($scores); // reverse array to show criteria 1 start from top when in graph
-
-$objPHPExcel->getActiveSheet()->getStyle("B12:F12")->getFont()->setBold(true);
+$objPHPExcel->getActiveSheet()->getStyle("B12:G12")->getFont()->setBold(true);
 $objPHPExcel->getActiveSheet()->getStyle("B4:B10")->getFont()->setBold(true);
 
 
+$objPHPExcel->getActiveSheet()->getStyle('B7')->getNumberFormat()->setFormatCode(PHPExcel_Style_NumberFormat::FORMAT_TEXT);
 
 /*csv border style*/
 $styleArray = array(
@@ -91,8 +91,9 @@ foreach(range('B','E') as $columnID) {
 $objPHPExcel->getActiveSheet()->getColumnDimension($columnID)
          ->setAutoSize(true);
 }
+$objPHPExcel->getActiveSheet()->getColumnDimension('C')->setAutoSize(false);
 
-$objPHPExcel->getActiveSheet()->getStyle("B$O:F".$i)->applyFromArray($styleArray);
+$objPHPExcel->getActiveSheet()->getStyle("B$O:G".$i)->applyFromArray($styleArray);
 unset($styleArray);
 
 $objWriter = new PHPExcel_Writer_Excel5($objPHPExcel);
--- classification/views/journal/render_detail_pdf.php
+++ classification/views/journal/render_detail_pdf.php
@@ -1,87 +1,54 @@
-<style type="text/css">
-		<!--
+<h3>Journal Classification Detail</h3><hr>
+<table cellspacing="3px" style="margin:20px;max-width: 100%; solid;position: relative;">
+	<tr>
+		<td class="tbold" width="100px;">Title</td><td><?php echo $journal['journal_name']; ?></td>
+	</tr>
+	<tr>
+		<td class="tbold">Dicipline</td><td><?php echo $disciplineTitle; ?></td>
+	</tr>
+	<tr>
+		<td class="tbold">Publisher</td><td><?php echo $journal['publisher']; ?></td>
+	</tr>
+	<tr >
+		<td class="tbold">Year Evaluate</td><td><?php echo $journal['year']; ?></td>
+	</tr>
+	<tr>
+		<td class="tbold">Form</td><td><?php echo $_GET['f'] ?></td>
+	</tr>
+	<tr>
+		<td class="tbold">Score</td><td><?php echo $journal['totalMarks'] . ' / ' . $fullMarks . ' (' . round(($journal['totalMarks'] / $fullMarks) * 100, 2) . '%)'; ?></td>
+	</tr>
 
-		.tbold {
-			font-weight: bold;
-		} 
-		
-		
-		td {
-		  padding:5px; 
-		  word-wrap:break-word;
-		} 
-		
-		th {
-		  padding:4px; 
-		} 
-		
-		table {
-			table-layout:fixed;
-		} 
-		
-		
-		
-		-->
-		</style>
- 
-		  <table cellspacing="3px" style="margin:20px;max-width: 100%; solid;position: relative;">
-              <tr>
-				<td class="tbold" width="100px;">Title</td><td><?php echo $journal['journal_name']; ?></td>
-              </tr>
-              <tr>
-				<td class="tbold">Dicipline</td><td><?php echo $disciplineTitle; ?></td>
-              </tr>
-			  <tr>
-				<td class="tbold">Publisher</td><td><?php echo $journal['publisher']; ?></td>
-              </tr>
-			   <tr >
-				<td class="tbold">Year Evaluate</td><td><?php echo $journal['year']; ?></td>
-              </tr>
-			   <tr>
-				<td class="tbold">Form</td><td><?php echo $_GET['f'] ?></td>
-              </tr>
-			  <tr>
-				<td class="tbold">Score</td><td><?php echo $journal['totalMarks'] . ' / ' . $fullMarks . ' (' . round(($journal['totalMarks'] / $fullMarks) * 100, 2) . '%)'; ?></td>
-              </tr>
-			   <tr>
-				<td class="tbold">Level</td><td><?php echo "-"; ?></td>
-              </tr>
-				
-			 </table>
-		  
-		  
-		 
-            
-            <table cellspacing="3px" style="width: 100%; border:1px solid;margin:20px;" class="table-list">
-         
-			   <tr bgcolor="#999999">
-					<th>&nbsp;</th>
-					<th>Criteria.</th>
-					<th>Choice</th>
-					<th>Score</th>
-					<th>Remarks</th>
-				</tr>
+</table>
+
+
+
+
+<table cellspacing="3px" style="width: 100%; border:1px solid;margin:20px;" class="table-list">
+
+	<tr bgcolor="#999999">
+		<th>&nbsp;</th>
+		<th>Criteria.</th>
+		<th>Choice</th>
+		<th>Score</th>
+		<th>Percentage</th>
+		<th>Remarks</th>
+	</tr>
 	<?php $i = 0 ?>
 	<?php $scores = [] ?>
-            <?php foreach ($journal['resultList'] as $row): ?>
-							<tr >
-								<td><?php echo ($i + 1) ?></td>
-								<td style="width: 45%; text-align: left;"><?php echo $row['criteria_name'] ?></td>
-								<td style="width: 20%; text-align: left;"><?php echo $row['choice_name'] ?></td>
-								<td style="width: 10%; text-align: left;"><?php echo $row['marks'] ?></td>
-								<td style="width: 15%; text-align: left;"><?php echo $row['remarks'] ?></td>
-								<?php
-								array_push($scores, [ 'value' => ($row['marks'] / $fullMarks * 100)]);
-								$i++;
-								?>
-							</tr>
-			<?php endforeach ?>
-			<?php $scores = array_reverse($scores) // reverse array to show criteria 1 start from top when in graph ?>
-     
-            </table>
- 
+	<?php foreach ($journal['resultList'] as $row): ?>
+		<tr >
+			<td><?php echo ($i + 1) ?></td>
+			<td style="width: 35%; text-align: left;"><?php echo $row['criteria_name'] ?></td>
+			<td style="width: 20%; text-align: left;"><?php echo $row['choice_name'] ?></td>
+			<td style="width: 10%; text-align: left;"><?php echo $row['marks'] . ' / ' . $row['totalCriteriaMarks'] ?></td>
+			<td style="width: 10%; text-align: left;"><?php echo round($row['marks'] / $row['totalCriteriaMarks'] * 100, 2) . '%' ?></td>
+			<td style="width: 15%; text-align: left;"><?php echo $row['remarks'] ?></td>
+			<?php
+			array_push($scores, [ 'value' => ($row['marks'] / $fullMarks * 100)]);
+			$i++;
+			?>
+		</tr>
+	<?php endforeach ?>
 
-		
-            
-
-
+</table>
